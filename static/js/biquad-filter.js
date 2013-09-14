b4589bb6e70d880410b223f00473f5e18a17677d
zapion <zapionator@gmail.com>
2013-09-15 01:50:15 +0800

    complete biquad-filter

--- static/js/biquad-filter.js
+++ static/js/biquad-filter.js
@@ -13,34 +13,42 @@ var biquadFilter = {
   play: function gt_play() {
     this.setupGraph();
     this.source.start(0);
+    //set all values as default
+    this.changeFrequency(this.biquadFilter.frequency.defaultValue);
+    this.changeDetune(this.biquadFilter.detune.defaultValue);
+    this.changeGain(this.biquadFilter.gain.defaultValue);
+    this.changeQ(0);
   },
   stop: function gt_stop() {
     this.source.stop(0);
   },
   changeFrequency: function gt_changeFrequency(val) {
     this.biquadFilter.frequency.value = val;
+    document.getElementById("frequency").nextSibling.innerHTML = val;
   },
   changeDetune: function gt_changeDetune(val) {
     this.biquadFilter.detune.value = val;
+    document.getElementById("detune").nextSibling.innerHTML = val + "%";
   },
   changeType: function gt_changeType(val) {
     this.biquadFilter.type = val;
   },
   changeGain: function gt_changeGain(val) {
     this.biquadFilter.gain.value = val;
+    document.getElementById("gain").nextSibling.innerHTML = val;
+  },
+  changeQ: function gt_changeQ(val) {
+    var input = Math.pow(10, val);
+    this.biquadFilter.Q.value = input;
+    document.getElementById("Q").nextSibling.innerHTML = input;
   },
   setupGraph: function gt_setupGraph() {
     this.source = this.context.createBufferSource();
     this.source.buffer = AudioSource.buffer[this.url];
     this.biquadFilter = this.context.createBiquadFilter();
-    var items = [];
-    for (var s in this.biquadFilter) {
-      items.push(s);
-      console.log(s, ": ", this.biquadFilter[s]);
-
+    for(var i in this.biquadFilter.frequency) {
+      console.log(i, ": ", this.biquadFilter.frequency[i]);
     }
-    items.sort();
-    
     this.analyser = this.context.createAnalyser();
     this.source.connect(this.biquadFilter);
     this.biquadFilter.connect(this.context.destination);
